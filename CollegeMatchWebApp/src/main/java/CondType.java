f94b7615eaaa233b8d6ee020f845b89c8b7b2f62
jsung23 <jsung1992@gmail.com>
2017-08-08 13:52:34 -0500

    final version with upstream master

--- CollegeMatchWebApp/src/main/java/CondType.java
+++ CollegeMatchWebApp/src/main/java/CondType.java
@@ -31,40 +31,5 @@ package main.java;
  *
  */
 public enum CondType {
-	NO_COND, RANGE, EQ, GT, GE, LT, LE, NE, LIKE, IN, OR_GROUP, REVERSE_IN
-}
-package main.java;
-
-/**
- * The type of condition being applied to a column
- * 
- * NO_COND
- * 		no condition for this column
- * RANGE
- * 		BETWEEN ? AND ?
- * EQ
- * 		= ?
- * GT
- * 		> ?
- * GE
- * 		>= ?
- * LT
- * 		< ?
- * LE
- * 		<= ?
- * NE
- * 		<> ?
- * LIKE
- * 		LIKE ?
- * IN
- * 		IN ?
- * 
- * OR_GROUP
- * 		(cond1 OR cond2 OR ...)
- * 
- * @author Geoff
- *
- */
-public enum CondType {
-	NO_COND, RANGE, EQ, GT, GE, LT, LE, NE, LIKE, IN, OR_GROUP, REVERSE_IN, DISTANCE
+	NO_COND, RANGE, EQ, GT, GE, LT, LE, NE, LIKE, IN, OR_GROUP, REVERSE_IN,  DISTANCE
 }
